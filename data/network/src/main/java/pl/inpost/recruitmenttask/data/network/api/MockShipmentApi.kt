5bab489ff350f7830fded26ab424d203ddd09fee
Dawid Dubiel <d.t.dubiel@gmail.com>
2024-04-26 15:31:49 +0200

    cleanup

--- data/network/src/main/java/pl/inpost/recruitmenttask/data/network/api/MockShipmentApi.kt
+++ data/network/src/main/java/pl/inpost/recruitmenttask/data/network/api/MockShipmentApi.kt
@@ -35,62 +35,9 @@ class MockShipmentApi(
 
         jsonAdapter.fromJson(json) as ShipmentsResponse
     }
-    private var firstUse = true
 
     override suspend fun getShipments(): List<ShipmentNetwork> {
         delay(1000)
         return response.shipments
     }
 }
-
-private fun mockShipmentNetwork(
-    number: String = Random.nextLong(1, 9999_9999_9999_9999).toString(),
-    type: ShipmentType = ShipmentType.PARCEL_LOCKER,
-    status: ShipmentStatus = ShipmentStatus.DELIVERED,
-    sender: CustomerNetwork? = mockCustomerNetwork(),
-    receiver: CustomerNetwork? = mockCustomerNetwork(),
-    operations: OperationsNetwork = mockOperationsNetwork(),
-    eventLog: List<EventLogNetwork> = emptyList(),
-    openCode: String? = null,
-    expireDate: ZonedDateTime? = null,
-    storedDate: ZonedDateTime? = null,
-    pickupDate: ZonedDateTime? = null
-) = ShipmentNetwork(
-    number = number,
-    shipmentType = type.name,
-    status = status.name,
-    eventLog = eventLog,
-    openCode = openCode,
-    expiryDate = expireDate,
-    storedDate = storedDate,
-    pickUpDate = pickupDate,
-    receiver = receiver,
-    sender = sender,
-    operations = operations
-)
-
-private fun mockCustomerNetwork(
-    email: String = "name@email.com",
-    phoneNumber: String = "123 123 123",
-    name: String = "Jan Kowalski"
-) = CustomerNetwork(
-    email = email,
-    phoneNumber = phoneNumber,
-    name = name
-)
-
-private fun mockOperationsNetwork(
-    manualArchive: Boolean = false,
-    delete: Boolean = false,
-    collect: Boolean = false,
-    highlight: Boolean = false,
-    expandAvizo: Boolean = false,
-    endOfWeekCollection: Boolean = false
-) = OperationsNetwork(
-    manualArchive = manualArchive,
-    delete = delete,
-    collect = collect,
-    highlight = highlight,
-    expandAvizo = expandAvizo,
-    endOfWeekCollection = endOfWeekCollection
-)
